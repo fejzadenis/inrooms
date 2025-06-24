25ac4ef8fc77d886941aeebd15f76ea056219d71
Denis Fejza <37412315+fejzadenis@users.noreply.github.com>
2025-06-25 01:21:16 +0200

    Updated HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -30,7 +30,7 @@ export function HomePage() {
                   </span>
                 </h1>
                 <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                  Meet people who provide you value by attending relationship nurturing video conferences. Connect with decision-makers, share opportunities, and accelerate your sales success.
+                  Meet people who provide you value by attending relationship nurturing video conferences. Connect with decision-makers, share opportunities, practice your pitch, and accelerate your sales success.
                 </p>
                 <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                   <div className="rounded-md shadow">
